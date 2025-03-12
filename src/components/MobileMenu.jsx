4db971a712ac3c9b2e564baad33551e67f1a66fd
flaviare1s <flaviarster@gmail.com>
2025-03-12 12:44:47 -0300

    Apaga comentários desnecessários

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -32,12 +32,10 @@ export const MobileMenu = () => {
         className="text-white"
       >
         {isMenuOpen ? (
-          // Ícone de "X" quando o menu estiver aberto
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         ) : (
-          // Ícone de hambúrguer quando o menu estiver fechado
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
