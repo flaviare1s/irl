9dd12df59ec332b16e2fec76c74b0fedf19bf42a
flaviare1s <flaviarster@gmail.com>
2025-03-13 15:34:36 -0300

    Ajusta estilo dos botões

--- src/components/BotaoDoacoes.jsx
+++ src/components/BotaoDoacoes.jsx
@@ -11,7 +11,7 @@ export const BotaoDoacoes = ({ text = 'Faça uma Doação', bgColor = 'bg-living
     <div>
       <button
         onClick={openModal}
-        className={`${bgColor} text-white font-bold w-[230px] h-[60px] rounded-full flex justify-start px-2 items-center lg:rotate-90 cursor-pointer`}>
+        className={`${bgColor} text-white font-bold w-[270px] h-[60px] rounded-full flex justify-start px-2 items-center lg:rotate-90 cursor-pointer`}>
         <div className='bg-radiant-orchid h-[50px] w-[50px] rounded-full flex justify-center items-center mr-2'>
           <FaHeart className="text-2xl text-freesia" />
         </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Button = ({ rota, text, bgColor }) => {
   return (
-    <Link to={rota} className={`${bgColor} text-white font-bold h-[50px] md:h-[60px] w-[270px] px-5 rounded-full flex justify-center items-center`}>
+    <Link to={rota} className={`${bgColor} text-white font-bold h-[60px] w-[270px] px-5 rounded-full flex justify-center items-center`}>
       {text}
     </Link>
   )
