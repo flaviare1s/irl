ff986529c1e542535b74ee2dfe0215963058d3ef
flaviare1s <flaviarster@gmail.com>
2025-03-14 11:16:41 -0300

    Ajusta margem de botão

--- src/components/BotaoDoacoes.jsx
+++ src/components/BotaoDoacoes.jsx
@@ -12,7 +12,7 @@ export const BotaoDoacoes = ({ text = 'Faça uma Doação', bgColor = 'bg-living
       <button
         onClick={openModal}
         className={`${bgColor} text-white font-bold w-[270px] h-[60px] rounded-full flex justify-start px-2 items-center lg:rotate-90 cursor-pointer hover:brightness-120`}>
-        <div className='bg-radiant-orchid h-[50px] w-[50px] rounded-full flex justify-center items-center mr-10'>
+        <div className='bg-radiant-orchid h-[50px] w-[50px] rounded-full flex justify-center items-center mr-8'>
           <FaHeart className="text-2xl text-freesia" />
         </div>
         {text}
