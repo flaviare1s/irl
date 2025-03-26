fecd1331ec1111cc5795509c0e9143d297a6a40c
flaviare1s <flaviarster@gmail.com>
2025-03-26 20:50:45 -0300

    Altera ícones da página de Programas

--- src/components/ProgramaComponente.jsx
+++ src/components/ProgramaComponente.jsx
@@ -6,7 +6,7 @@ export const ProgramaComponente = ({ img, nome, texto, imagens }) => {
 
   return (
     <section className="flex flex-col items-center justify-center p-10 lg:p-4 h-full">
-      <img className="mb-5 w-[150px] rounded-lg" src={img} alt={nome} />
+      <img className="mb-5 w-[150px] h-[150px]" src={img} alt={nome} />
       <div className="flex-grow flex flex-col items-center">
         <h3 className="text-xl md:text-2xl font-bold text-center mb-5">{nome}</h3>
         <p className="text-lg font-medium text-center">{texto}</p>
