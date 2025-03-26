8e71358e4cd6df682084bbcb6c7cfde5ba2f1f2b
flaviare1s <flaviarster@gmail.com>
2025-03-26 15:05:28 -0300

    Ajusta cursor pointer

--- src/components/ProgramaComponente.jsx
+++ src/components/ProgramaComponente.jsx
@@ -6,7 +6,7 @@ export const ProgramaComponente = ({ img, nome, descricao }) => {
         <h3 className="text-xl md:text-2xl font-bold text-center mb-5">{nome}</h3>
         <p className="text-lg font-medium text-center">{descricao}</p>
       </div>
-      <button className="h-8 w-8 md:h-10 md:w-10 rounded-full bg-primary flex justify-center items-center text-white text-2xl md:text-3xl font-bold mt-5">+</button>
+      <button className="h-8 w-8 md:h-10 md:w-10 rounded-full bg-primary flex justify-center items-center text-white text-2xl md:text-3xl font-bold mt-5 cursor-pointer">+</button>
     </section>
   )
 }
