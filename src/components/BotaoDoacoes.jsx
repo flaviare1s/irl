b27fbc716b91595a7f5107c3dc98ed07a490be5c
flaviare1s <flaviarster@gmail.com>
2025-04-12 11:34:06 -0300

    Adiciona estilo de hover no botão de doações

--- src/components/BotaoDoacoes.jsx
+++ src/components/BotaoDoacoes.jsx
@@ -11,8 +11,8 @@ export const BotaoDoacoes = ({ text = 'Faça uma Doação', bgColor = 'bg-living
     <div>
       <button
         onClick={openModal}
-        className={`${bgColor} text-white font-bold w-[270px] h-[60px] rounded-full flex justify-start px-2 items-center lg:rotate-90 cursor-pointer hover:brightness-120`}>
-        <div className='bg-radiant-orchid h-[50px] w-[50px] rounded-full flex justify-center items-center mr-8'>
+        className={`${bgColor} text-white font-bold w-[270px] h-[60px] rounded-full flex justify-start px-2 items-center lg:rotate-90 cursor-pointer hover:brightness-120 group transition-all duration-300`}>
+        <div className="bg-radiant-orchid h-[50px] w-[50px] rounded-full flex justify-center items-center mr-8 transform transition-transform duration-300 group-hover:translate-x-3">
           <FaHeart className="text-2xl text-freesia" />
         </div>
         {text}
