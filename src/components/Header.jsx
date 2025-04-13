5f3fe9a35186124f8762a3eeff527d1ddd3dea3f
flaviare1s <flaviarster@gmail.com>
2025-04-13 08:35:55 -0300

    Ajusta tamanho do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ export const Header = () => {
   const closeModal = () => setIsOpen(false);
 
   return (
-    <header className="bg-primary text-white flex flex-col justify-center">
-      <nav className="flex justify-between items-center py-2 px-5 md:px-10 md:py-4">
-        <div className="h-[80px] w-[120px] md:h-[100px] md:w-[150px]">
+    <header className="bg-primary text-white flex flex-col justify-center it">
+      <nav className="flex justify-between items-center px-5 md:px-10 md:py-2">
+        <div className="h-[80px] w-[100px] md:h-[80px] md:w-[120px] flex justify-center items-center">
           <Link to='/'><img className="w-full" src={Logo} alt="Logo do IRL" /></Link>
         </div>
         <div>
