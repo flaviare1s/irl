8888585d8f2c649d4b81d52bbdd936ab30aaaeb0
flaviare1s <flaviarster@gmail.com>
2025-05-10 15:23:33 -0300

    Ajusta estilo

--- src/pages/FacaParte.jsx
+++ src/pages/FacaParte.jsx
@@ -7,7 +7,7 @@ export const FacaParte = () => {
     <div className="flex flex-col justify-center items-center">
       <section className="relative flex flex-col md:flex-row items-center justify-center gap-10 py-14 px-4 md:px-10 lg:px-20 xl:px-[200px] bg-gray-50">
         <div className="absolute top-10 left-10 hidden md:block"><img src={elementoGrafico} className="h-[100px]" /></div>
-        <div className="w-full md:w-1/2 space-y-6">
+        <div className="w-full md:w-1/2 space-y-6 font-medium">
           <h2 className="text-3xl md:text-5xl font-bold text-primary text-center md:text-left">
             Como nos encontrar
           </h2>
