30dee50b8a7e8b899279d65a87c77d78fa4880a4
flaviare1s <flaviarster@gmail.com>
2025-05-10 15:19:44 -0300

    Adiciona elemento gráfico para estilizar

--- src/pages/FacaParte.jsx
+++ src/pages/FacaParte.jsx
@@ -1,8 +1,10 @@
 import { Contato } from "../components/Contato";
+import elementoGrafico from "../assets/img/elementos/x.png";
 
 export const FacaParte = () => {
   return (
-    <section className="flex flex-col md:flex-row items-center justify-center gap-10 py-14 px-4 md:px-10 lg:px-20 xl:px-[200px] bg-gray-50">
+    <section className="relative flex flex-col md:flex-row items-center justify-center gap-10 py-14 px-4 md:px-10 lg:px-20 xl:px-[200px] bg-gray-50">
+      <div className="absolute top-10 left-10 hidden md:block"><img src={elementoGrafico} className="h-[100px]" /></div>
       <div className="w-full md:w-1/2 space-y-6">
         <h2 className="text-3xl md:text-5xl font-bold text-primary text-center md:text-left">
           Como nos encontrar
