48b0bbfdc1944edf7703d062dc604ce9480ac148
flaviare1s <flaviarster@gmail.com>
2025-05-10 08:32:52 -0300

    Ajusta página de programas

--- src/components/Programa.jsx
+++ src/components/Programa.jsx
@@ -1,18 +1,27 @@
-export const Programa = ({ id, img, titulo, paragrafos, bgColor }) => {
+export const Programa = ({ id, img, titulo, paragrafos, bgColor, color, isReverse, elementoGrafico1, elementoGrafico2, elementoGrafico3 }) => {
   return (
-    <section
-      id={id}
-      className={`bg-${bgColor} text-white grid grid-cols-1 md:grid-cols-2 p-10 md:px-44 md:py-20 items-center scroll-mt-20`}
-    >
-      <div className="w-[80%]">
-        <img src={img} alt="Imagem do programa" />
+      <section
+        id={id}
+        className={`bg-${bgColor} text-${color} flex flex-col ${isReverse ? 'md:flex-row-reverse' : 'md:flex-row'} justify-center gap-10 p-10 md:px-44 md:py-20 items-center scroll-mt-20 relative`}
+      >
+      <div className="hidden md:block absolute top-10 left-5">
+        <img className="h-20 md:h-32" src={elementoGrafico1} />
       </div>
-      <div>
-        <h2 className="text-3xl md:text-5xl font-bold mb-5">{titulo}</h2>
-        {paragrafos.map((texto, index) => (
-          <p key={index} className="mb-4">{texto}</p>
-        ))}
+      <div className="hidden md:block absolute top-4 right-20">
+        <img className="h-20 md:h-32" src={elementoGrafico2} />
       </div>
-    </section>
+      <div className="hidden md:block absolute top-1/2 left-10">
+        <img className="h-20 md:h-32" src={elementoGrafico3} />
+      </div>
+        <div className="max-w-[500px]  w-full flex justify-center">
+          <img className="w-[90%] h-auto rounded-2xl" src={img} alt="Imagem do programa" />
+        </div>
+        <div className="max-w-[500px]">
+          <h2 className="text-3xl md:text-5xl font-bold my-5">{titulo}</h2>
+          {paragrafos.map((texto, index) => (
+            <p key={index} className="mb-4">{texto}</p>
+          ))}
+        </div>
+      </section>
   );
 };
--- src/pages/Programas.jsx
+++ src/pages/Programas.jsx
@@ -1,13 +1,22 @@
 import acolhendo from "../assets/img/fotos/programas/Acolhendo6.jpg";
 import mulheres from "../assets/img/fotos/programas/Mulheres.jpg";
 import agroflorestal from "../assets/img/fotos/programas/Agrofloresta.jpg";
+import elementoGrafico1 from "../assets/img/elementos/elemento-verde2.png";
+import elementoGrafico2 from "../assets/img/elementos/elemento-azul2.png";
+import elementoGrafico3 from "../assets/img/elementos/elemento-rosa3.png";
+import elementoGrafico4 from "../assets/img/elementos/x.png";
+
+
 import { Programa } from "../components/Programa";
+import { LoadingScreen } from "../components/LoadingScreen";
+import { useState } from "react";
 
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export const Programas = () => {
   const location = useLocation();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (location.hash) {
@@ -21,13 +30,26 @@ export const Programas = () => {
     }
   }, [location]);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div>
+      {isLoading && <LoadingScreen />}
       <div id="acolhendo-e-convivendo">
         <Programa
           id="acolhendo-e-convivendo"
           bgColor={"radiant-orchid"}
+          color={"white"}
+          isReverse={false}
           img={acolhendo}
+          elementoGrafico1={elementoGrafico4}
+          elementoGrafico2={elementoGrafico2}
           titulo="Programa Acolhendo e Convivendo"
           paragrafos={[
             "O Programa A&C funciona no contraturno escolar e atende crianças e adolescentes de 6 a 13 anos de idade, moradores do Bairro Ellery e adjacências, em Fortaleza/CE.",
@@ -36,26 +58,32 @@ export const Programas = () => {
           ]}
         />
       </div>
-      <div id="grupo-de-mulheres-francisca-clotilde">
-        <Programa
-          id="grupo-de-mulheres-francisca-clotilde"
-          bgColor={"primary"}
-          img={mulheres}
-          titulo="Grupo de Mulheres Francisca Clotilde"
-          paragrafos={[
-            "O Grupo de mulheres Francisca Clotilde é um serviço de convivência e fortalecimento de vínculos para figuras femininas responsáveis pelas crianças e adolescentes inscritas no Programa Acolhendo e Convivendo. Os encontros são semanais e tem como objetivo ampliar o universo informacional das mulheres participantes no que se refere a sustentabilidade, a saúde e direitos da mulher, assim como estimular a autonomia financeira e a inserção no mercado de trabalho. Além disso, em parceria com o Sesc, por meio do Programa Sesc Ativo, são oferecidas aulas de dança duas vezes por semana, através de uma turma de multipráticas de dança."
-          ]}
-        />
-      </div>
       <div id="agrofloresta">
         <Programa
           id="agrofloresta"
           bgColor={"greenery"}
+          color={"black"}
+          elementoGrafico3={elementoGrafico3}
+          isReverse={true}
           img={agroflorestal}
           titulo="Sistema Agroflorestal - SAF"
           paragrafos={[
             "O sistema agroflorestal (SAF) é uma técnica de recuperação ambiental e de produção de alimentos que se inspira na dinâmica que ocorre na natureza, onde plantas de diversas espécies vivem juntas, uma ajudando a outra no seu desenvolvimento. ",
             "Com a implementação do sistema agroflorestal do IRL, busca-se oportunizar um contato próximo das crianças e adolescentes com a natureza. No SAF são realizadas atividades educativas, e também, momentos de cultivo e plantio a fim de fomentar e promover a segurança alimentar, além do incentivo à uma vida sustentável e harmoniosa entre seres humanos e meio ambiente."
+          ]}
+        />
+      </div>
+      <div id="grupo-de-mulheres-francisca-clotilde">
+        <Programa
+          id="grupo-de-mulheres-francisca-clotilde"
+          bgColor={"primary"}
+          color={"white"}
+          elementoGrafico2={elementoGrafico1}
+          isReverse={false}
+          img={mulheres}
+          titulo="Grupo de Mulheres Francisca Clotilde"
+          paragrafos={[
+            "O Grupo de mulheres Francisca Clotilde é um serviço de convivência e fortalecimento de vínculos para figuras femininas responsáveis pelas crianças e adolescentes inscritas no Programa Acolhendo e Convivendo. Os encontros são semanais e tem como objetivo ampliar o universo informacional das mulheres participantes no que se refere a sustentabilidade, a saúde e direitos da mulher, assim como estimular a autonomia financeira e a inserção no mercado de trabalho. Além disso, em parceria com o Sesc, por meio do Programa Sesc Ativo, são oferecidas aulas de dança duas vezes por semana, através de uma turma de multipráticas de dança."
           ]}
         />
       </div>
