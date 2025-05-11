f3af4e55021fd7836def97adcabe74d29e6c74e5
flaviare1s <flaviarster@gmail.com>
2025-05-11 17:30:00 -0300

    Ajusta número de whatsapp

--- src/components/BotaoWhatsapp.jsx
+++ src/components/BotaoWhatsapp.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const BotaoWhatsapp = () => {
   return (
     <a
-      href="https://wa.me/5585985596683"
+      href="https://wa.me/5585991464053"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-4 right-4 z-50 bg-greenery hover:bg-white text-white hover:text-greenery border hover:border-greenery rounded-full p-4 shadow-lg transition-all duration-300"
