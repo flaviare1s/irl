762819a82c7d91f1063a514c151566e2975e7fb1
flaviare1s <flaviarster@gmail.com>
2025-05-21 16:33:46 -0300

    Ajuste de hover

--- src/components/ProgramaComponente.jsx
+++ src/components/ProgramaComponente.jsx
@@ -17,7 +17,7 @@ export const ProgramaComponente = ({ img, nome, texto, id }) => {
 
       <button
         onClick={handleClick}
-        className="h-8 w-8 md:h-10 md:w-10 rounded-full bg-primary flex justify-center items-center text-white text-2xl md:text-3xl font-bold mt-5 cursor-pointer"
+        className="h-8 w-8 md:h-10 md:w-10 rounded-full bg-primary flex justify-center items-center text-white text-2xl md:text-3xl font-bold mt-5 cursor-pointer hover:brightness-110 transition duration-300"
         aria-label={`Ir para o programa ${nome}`}
       >
         +
