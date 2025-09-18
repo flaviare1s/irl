fd314100e78f3074aadf24a75e7b66cd15c3cfd8
flaviare1s <flaviarster@gmail.com>
2025-09-18 14:51:03 -0300

    Ajusta números de 2024

--- src/components/Numeros.jsx
+++ src/components/Numeros.jsx
@@ -13,12 +13,12 @@ export const Numeros = () => {
       </div>
       <div className="flex flex-col lg:flex-row justify-center items-center">
         <div className="grid grid-cols-2 md:grid-cols-3 md:pr-[200px] gap-8 lg:min-h-[400px]">
-          <Numero textColor="text-primary" numero='93' descricao='Crianças e adolescentes atendidos com idades entre 6 e 15 anos' />
-          <Numero textColor="text-freesia" numero='68' descricao='Famílias diretamente beneficiadas' />
-          <Numero textColor="text-greenery" numero='1144' descricao='Aulas de educação social e educação formal' />
-          <Numero textColor="text-living-coral" numero='1119' descricao='Aulas de arte-educação' />
-          <Numero textColor="text-radiant-orchid" numero='254' descricao='Atividades agroflorestais' />
-          <Numero textColor="text-primary" numero='42' descricao='Encontros formativos do Grupo de Mulheres Francisca Clotilde' />
+          <Numero textColor="text-primary" numero='85' descricao='Crianças e adolescentes participantes' />
+          <Numero textColor="text-freesia" numero='62' descricao='Famílias diretamente beneficiadas' />
+          <Numero textColor="text-greenery" numero='988' descricao='Aulas de educação social e educação formal' />
+          <Numero textColor="text-living-coral" numero='1140' descricao='Aulas de arte-educação e esporte' />
+          <Numero textColor="text-primary" numero='38' descricao='Encontros formativos do Grupo de Mulheres Francisca Clotilde' />
+          <Numero textColor="text-primary" numero='23.920' descricao='Refeições servidas' />
         </div>
         <div className="mt-10">
           <BotaoDoacoes />
