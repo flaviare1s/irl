35d14a246f334a45d428a0209e3cd726f80c40f4
flaviare1s <flaviarster@gmail.com>
2026-08-04 06:39:57 -0300

    Adiciona link de Home nos menus

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 export const DesktopMenu = () => {
   return (
     <div className="hidden gap-7 sm:flex md:text-lg font-bold">
+      <Link className="link-nav" to='/'>Home</Link>
       <Link className="link-nav" to='/programas'>Programas</Link>
       <Link className="link-nav" to='/transparencia'>Transparência</Link>
       <Link className="link-nav" to='/participe'>Faça parte</Link>
