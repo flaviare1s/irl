75fe2a7678ed720130113e04f1c2f59cf044fe6a
flaviare1s <flaviarster@gmail.com>
2026-08-04 06:34:02 -0300

    Ajuste nas animaçõe

--- src/components/Programa.jsx
+++ src/components/Programa.jsx
@@ -7,7 +7,7 @@ export const Programa = ({ id, img, titulo, paragrafos, bgColor, color, isRevers
       <section
         id={id}
         ref={revelar.ref}
-        className={`${revelar.className} bg-${bgColor} text-${color} flex flex-col ${isReverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} justify-center gap-10 p-10 md:px-44 md:py-20 items-center scroll-mt-20 relative`}
+        className={`bg-${bgColor} text-${color} flex flex-col ${isReverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} justify-center gap-10 p-10 md:px-44 md:py-20 items-center scroll-mt-20 relative`}
       >
       <div className="hidden md:block absolute top-10 left-5">
         <img loading="lazy" className="h-20 md:h-32" src={elementoGrafico1} alt="" />
@@ -18,7 +18,7 @@ export const Programa = ({ id, img, titulo, paragrafos, bgColor, color, isRevers
       <div className="hidden md:block absolute top-1/2 left-10">
         <img loading="lazy" className="h-20 md:h-32" src={elementoGrafico3} alt="" />
       </div>
-        <div className="max-w-[500px]  w-full flex justify-center">
+        <div className={`${revelar.className} max-w-[500px]  w-full flex justify-center`}>
           <img
             className="w-[90%] h-auto rounded-2xl"
             src={img}
@@ -27,7 +27,7 @@ export const Programa = ({ id, img, titulo, paragrafos, bgColor, color, isRevers
             {...(prioridade ? { fetchPriority: "high" } : { loading: "lazy" })}
           />
         </div>
-        <div className="max-w-[500px]">
+        <div className={`${revelar.className} max-w-[500px]`}>
           <h2 className="text-3xl md:text-5xl font-bold my-5">{titulo}</h2>
           {paragrafos.map((texto, index) => (
             <p key={index} className="mb-4">{texto}</p>
